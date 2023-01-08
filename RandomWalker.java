13265e9974d26c938e9a755eccd86c08f5132689
yousefnaam <69697080+yousefnaam@users.noreply.github.com>
2023-01-08 15:39:13 -0500

    Create RandomWalker.java

---
+++ RandomWalker.java
@@ -0,0 +1,25 @@
+public class RandomWalker
+
+{
+    public static void main(String[] args)
+    {
+        int steps = Integer.parseInt(args[0]);
+
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < steps; i++)
+        {
+            double rand = Math.random();
+            if(rand < 0.25)
+                x++; 
+            else if(rand < 0.5)
+                y--; 
+            else if(rand < 0.75)
+                x--; 
+            else if(rand < 1.0)
+                y++;
+            System.out.println("(" + x + ", " + y + ")");
+        }
+        System.out.println("squared distance = " + (double)(x*x + y*y));
+    }
+}
